41dd8ce4a616c094236ab4b07c31c718219fe521
Scott Phillpott <scott@phillpott.com>
2019-12-19 08:20:58 -0500

    successful import of PDF

--- NLTK_env/nltk_one_pdf.py
+++ NLTK_env/nltk_one_pdf.py
@@ -2,11 +2,27 @@
 
 import nltk
 import PyPDF2
+import textblob as TextBlob
+import spacy
+
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
 
 mypdf = open("/home/scott/projects/RedTideResearch/dataHarvests/COHH-impleplan_31823.pdf", mode="rb")
+mypdf2 = open("/home/scott/projects/RedTideResearch/dataHarvests/Blasco-1977-Limnology_and_Oceanography.pdf", mode="rb")
 
 pdf_document = PyPDF2.PdfFileReader(mypdf)
+pdf_document2 = PyPDF2.PdfFileReader(mypdf2)
 
-first_page = pdf_document.getPage(3)
+first_page = pdf_document.getPage(4)
+first_page2 = pdf_document2.getPage(0)
 
 print(first_page.extractText())
+print("-----------------second pdf --------------------------------------")
+print(first_page2.extractText())
+
+print("++++++++++++++++ sent tokenize +++++++++++++++++++++++++++++++++++")
+
+print(sent_tokenize(pdf_document2))
